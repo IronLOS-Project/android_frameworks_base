cc447e0b5449afb153d635159c4a0946d9c776c7
ezio84 <brabus84@gmail.com>
2019-07-26 12:45:22 +0300

    Less boring heads up option [1/2]
    allow to show heads up only for dialer and messaging
    app when they are not in foreground
    
    AICPfied: disabled by default
    
    Less annoying heads up: code fixes and improvements
    
    fix Ambient Display not working when disabling heads up
    remove the foreground app check, it's good to show heads up
    when Dialer is in foreground instead of incallui
    
    Change-Id: I08b035f5ba17a7964dc0d13385afb7d52fbfb395
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5186,6 +5186,12 @@ public final class Settings {
         public static final String NOTIFICATION_PULSE_DURATION = "ambient_notification_light_duration";
 
         /**
+         * Whether to show heads up only for dialer and sms apps
+         * @hide
+         */
+        public static final String LESS_BORING_HEADS_UP = "less_boring_heads_up";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -5316,6 +5322,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(SHOW_FOURG);
             PRIVATE_SETTINGS.add(HEADS_UP_STOPLIST_VALUES);
             PRIVATE_SETTINGS.add(HEADS_UP_BLACKLIST_VALUES);
+            PRIVATE_SETTINGS.add(LESS_BORING_HEADS_UP);
         }
 
         /**

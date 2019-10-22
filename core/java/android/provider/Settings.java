7427dddff1746716aff75c648435b04b9e4a0609
Dil3mm4 <dil3mm4.dev@gmail.com>
2019-10-22 04:59:15 +0200

    Feature: Don't touch my headsup (1/2)
    AshutoshSundresh:
    * properly document
    * change setting name
    * add to backup
    
    Change-Id: If876fc2f7a8e3860b1f6c0088e19d91110cd40f9

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5183,6 +5183,12 @@ public final class Settings {
         public static final String LESS_BORING_HEADS_UP = "less_boring_heads_up";
 
         /**
+         * Whether to avoid launching the app on touching heads-up
+         * @hide
+         */
+        public static final String DONT_TOUCH_HEADSUP = "dont_touch_headsup";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -5314,6 +5320,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(HEADS_UP_STOPLIST_VALUES);
             PRIVATE_SETTINGS.add(HEADS_UP_BLACKLIST_VALUES);
             PRIVATE_SETTINGS.add(LESS_BORING_HEADS_UP);
+            PRIVATE_SETTINGS.add(DONT_TOUCH_HEADSUP);
         }
 
         /**

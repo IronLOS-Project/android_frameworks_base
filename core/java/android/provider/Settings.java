56f5bb7e8da8053c9f3630848e742e79f1d568f5
ezio84 <brabus84@gmail.com>
2020-11-08 00:06:52 +0100

    ScreenOFF volume skiptrack (with remote Spotify support)
    and other players that allow music controls for remote devices (PC, PS4)
    through the media notification
    
    Change-Id: I007f97e6f8d3e0d3a091e326c13b9b7bdd4a6646

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4825,6 +4825,11 @@ public final class Settings {
         public static final String ADAPTIVE_PLAYBACK_TIMEOUT = "adaptive_playback_timeout";
 
         /**
+         * @hide
+         */
+        public static final String VOLUME_BUTTON_MUSIC_CONTROL = "volume_button_music_control";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *

ca22c843c05ee6e9f94559b0d21497fcc3fa7896
ezio84 <brabus84@gmail.com>
2021-06-04 09:06:52 +0000

    Advanced nav gestures: improve code and fix glitches
    fix  arrows animation glitch after triggering an action
    with the extended swipe
    
    hide the arrows before triggering a screenshot action
    
    code cleanup
    
    Change-Id: I5f01938b6b688acd14225f58ce5a2032f083f92f
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarEdgePanel.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarEdgePanel.java
@@ -674,8 +674,7 @@ public class NavigationBarEdgePanel extends View implements NavigationEdgeBackPl
         }
     }
 
-    @Override
-    public void resetOnDown() {
+    private void resetOnDown() {
         animate().cancel();
         mAngleAnimation.cancel();
         mTranslationAnimation.cancel();

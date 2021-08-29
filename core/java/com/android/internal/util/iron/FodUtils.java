vijaymalav564/android_frameworks_base-2

Use FodUtils for FOD config checking
base: Add our EvolutionUtils helper class

From time to time we need to filter out certain features because not every
device is created equal. This makes it easy to call from one place instead
of adding the same code time and time again.

As of right now this class includes checks for

- Fingerprint support
- Fingerprint support & enrollment
- NFC
- WiFi
- Camera
- Bluetooth
- Mobile connection
- Alterative ambient display (OnePlus devices)
- A/B device support

@gwolf2u: keep LOS overlay

Change-Id: Ia27dc8d49a78c843f2a185f3aef195f17cc090aa
Signed-off-by: Saikiran <bjsaikiran@gmail.com>
Signed-off-by: Joey Huab <joey@evolution-x.org>

core/java/com/android/internal/util/crdroid/FodUtils.java
@@ -1,0 +1,25 @@
+/*
+ * Copyright (C) 2019-2020 The Evolution X Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.crdroid;
+
+import android.content.Context;
+
+public class FodUtils {
+    public static boolean hasFodSupport(Context context) {
+        return context.getResources().getBoolean(com.android.internal.R.bool.config_needCustomFODView);
+    }
+}

packages/SystemUI/src/com/android/systemui/biometrics/FODCircleViewImpl.java
@@ -21,6 +21,8 @@ import android.content.pm.PackageManager;
 import android.util.Slog;
 import android.view.View;
 
+import com.android.internal.util.crdroid.FodUtils;
+
 import com.android.systemui.SystemUI;
 import com.android.systemui.statusbar.CommandQueue;
 
@@ -46,7 +48,7 @@ public class FODCircleViewImpl extends SystemUI implements CommandQueue.Callback
     public void start() {
         PackageManager packageManager = mContext.getPackageManager();
         if (!packageManager.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT) ||
-                !packageManager.hasSystemFeature(LineageContextConstants.Features.FOD)) {
+                !(packageManager.hasSystemFeature(LineageContextConstants.Features.FOD) || FodUtils.hasFodSupport(mContext))) {
             return;
         }
         mCommandQueue.addCallback(this);

IronLOS-Project/android_frameworks_base

core/java/com/android/internal/util/iron/FodUtils.java
@@ -1,0 +1,25 @@
+/*
+ * Copyright (C) 2019-2021 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.iron;
+
+import android.content.Context;
+
+public class FodUtils {
+    public static boolean hasFodSupport(Context context) {
+        return context.getResources().getBoolean(com.android.internal.R.bool.config_needCustomFODView);
+    }
+}

packages/SystemUI/src/com/android/systemui/biometrics/FODCircleViewImpl.java
@@ -21,6 +21,7 @@ import android.content.pm.PackageManager;
 import android.util.Slog;
 import android.view.View;
 
+import com.android.internal.util.iron.FodUtils;
 import com.android.systemui.SystemUI;
 import com.android.systemui.biometrics.FODCircleViewImplCallback;
 import com.android.systemui.statusbar.CommandQueue;
@@ -55,7 +56,7 @@ public class FODCircleViewImpl extends SystemUI implements CommandQueue.Callback
     public void start() {
         PackageManager packageManager = mContext.getPackageManager();
         if (!packageManager.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT) ||
-                !packageManager.hasSystemFeature(LineageContextConstants.Features.FOD)) {
+                !(packageManager.hasSystemFeature(LineageContextConstants.Features.FOD) || FodUtils.hasFodSupport(mContext))) {
             return;
         }
         mCommandQueue.addCallback(this);
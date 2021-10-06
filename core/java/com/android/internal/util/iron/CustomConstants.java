PixelExtended/frameworks_base

fwb: Introduce CustomConstants for different global variables

core/java/com/android/internal/util/custom/CustomConstants.java
@@ -1,0 +1,34 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.custom;
+
+import android.annotation.SdkConstant;
+
+public final class CustomConstants {
+    /**
+     * Features supported by ShapeShiftOS.
+     */
+    public static class Features {
+        /**
+         * Feature for {@link PackageManager#getSystemAvailableFeatures} and
+         * {@link PackageManager#hasSystemFeature}: The device includes the Lineage
+         * fingerprint in screen utilized by the lineage sdk.
+         */
+        @SdkConstant(SdkConstant.SdkConstantType.FEATURE)
+        public static final String FOD = "vendor.lineage.biometrics.fingerprint.inscreen";
+    }
+}

IronLOS-Project/android_frameworks_base

Change-Id: I78a7d1fc33e5848951c55ceb8e518e16c3105307

core/java/com/android/internal/util/iron/CustomConstants.java
@@ -1,0 +1,34 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.iron;
+
+import android.annotation.SdkConstant;
+
+public final class CustomConstants {
+    /**
+     * Features supported by ShapeShiftOS.
+     */
+    public static class Features {
+        /**
+         * Feature for {@link PackageManager#getSystemAvailableFeatures} and
+         * {@link PackageManager#hasSystemFeature}: The device includes the Lineage
+         * fingerprint in screen utilized by the lineage sdk.
+         */
+        @SdkConstant(SdkConstant.SdkConstantType.FEATURE)
+        public static final String FOD = "vendor.lineage.biometrics.fingerprint.inscreen";
+    }
+}